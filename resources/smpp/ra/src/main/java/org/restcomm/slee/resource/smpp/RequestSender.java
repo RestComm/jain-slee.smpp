436982406e219db7aca5e2d82b4b8756b19a2aae
Anatoly Satanovskiy <anatoly.satanovskiy@mobius-software.com>
2017-06-14 14:57:34 +0300

    changed catch interruptedexception on smpp senders interrupt

--- resources/smpp/ra/src/main/java/org/restcomm/slee/resource/smpp/RequestSender.java
+++ resources/smpp/ra/src/main/java/org/restcomm/slee/resource/smpp/RequestSender.java
@@ -83,17 +83,18 @@ public class RequestSender extends Thread {
                                 null, e, false);
                     }
                 }
+            } catch(InterruptedException ex) { 
+            	//that should be legal if queue empty or we are stopping
             } catch (Exception e) {
                 tracer.severe("Exception when sending of sendRequestPdu: " + e.getMessage(), e);
                 if (task != null) {
                     fireSendPduStatusEvent(EventsType.SEND_PDU_STATUS, task.getSmppServerTransaction(), task.getRequest(),
                             null, e, false);
                 }
-            }
-            finally
-            {
-            	if(task!=null)
+            } finally {
+            	if(task!=null) {
             		task.getSmppServerTransaction().releaseSemaphore();
+            	}
             }
         }
     }
--- resources/smpp/ra/src/main/java/org/restcomm/slee/resource/smpp/ResponseSender.java
+++ resources/smpp/ra/src/main/java/org/restcomm/slee/resource/smpp/ResponseSender.java
@@ -73,17 +73,18 @@ public class ResponseSender extends Thread {
                         countSendResponsePdu(smppSessionCounters, task.getResponse(), responseTime, responseTime);
                     }
                 }
+            } catch(InterruptedException ex) { 
+            	//that should be legal if queue empty or we are stopping
             } catch (Exception e) {
                 tracer.severe("Exception when sending of sendResponsePdu: " + e.getMessage(), e);
                 if (task != null) {
                     fireSendPduStatusEvent(EventsType.SEND_PDU_STATUS, task.getSmppServerTransaction(), task.getRequest(),
                             task.getResponse(), e, false);
                 }
-            }
-            finally
-            {
-            	if(task!=null)
+            } finally {
+            	if(task!=null) {
             		task.getSmppServerTransaction().releaseSemaphore();
+            	}
             }
         }
     }
