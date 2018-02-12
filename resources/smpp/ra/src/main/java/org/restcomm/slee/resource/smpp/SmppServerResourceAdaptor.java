ce463b42fe242756a2f7f9101e86eae0813d3e2b
Anatoly Satanovskiy <anatoly.satanovskiy@mobius-software.com>
2018-02-12 17:00:02 +0200

    removing LB heartbeat feature

--- resources/smpp/ra/src/main/java/org/restcomm/slee/resource/smpp/SmppServerResourceAdaptor.java
+++ resources/smpp/ra/src/main/java/org/restcomm/slee/resource/smpp/SmppServerResourceAdaptor.java
@@ -3,11 +3,7 @@ package org.restcomm.slee.resource.smpp;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
@@ -35,8 +31,6 @@ import javax.slee.resource.StartActivityException;
 import javax.slee.resource.UnrecognizedActivityHandleException;
 
 import org.jboss.mx.util.MBeanServerLocator;
-import org.restcomm.slee.resource.smpp.heartbeat.SmppLoadBalancerHeartBeatingService;
-import org.restcomm.slee.resource.smpp.heartbeat.SmppLoadBalancerHeartBeatingServiceImpl;
 import org.restcomm.smpp.SmppManagement;
 
 public class SmppServerResourceAdaptor implements ResourceAdaptor {
@@ -52,9 +46,6 @@ public class SmppServerResourceAdaptor implements ResourceAdaptor {
     private transient static final Address address = new Address(AddressPlan.IP, "localhost");
 
     private ConcurrentHashMap<SmppTransactionImpl, DelayedActivityEndTask> activityEndTasks = new ConcurrentHashMap<SmppTransactionImpl, DelayedActivityEndTask>();
-
-    private Properties loadBalancerHeartBeatingServiceProperties;
-    private SmppLoadBalancerHeartBeatingService loadBalancerHeartBeatingService;
 
     public SmppServerResourceAdaptor() {
         // TODO Auto-generated constructor stub
@@ -181,11 +172,6 @@ public class SmppServerResourceAdaptor implements ResourceAdaptor {
                 if (tracer.isInfoEnabled()) {
                     tracer.info("Activated RA Entity " + this.raContext.getEntityName());
                 }
-
-                if(loadBalancerHeartBeatingServiceProperties != null) {
-                    loadBalancerHeartBeatingService = initHeartBeatingService(mBeanServer);
-                    loadBalancerHeartBeatingService.start();
-                }
             } else {
                 if (object != null) {
                     if (tracer.isWarningEnabled()) {
@@ -215,11 +201,6 @@ public class SmppServerResourceAdaptor implements ResourceAdaptor {
         if (tracer.isFineEnabled()) {
             tracer.fine("Configuring RA Entity " + this.raContext.getEntityName());
         }
-        try {
-            loadBalancerHeartBeatingServiceProperties = prepareHeartBeatingServiceProperties(properties);
-        } catch (InvalidConfigurationException e) {
-            tracer.severe("Configuring of SMPP RA failed ", e);
-        }
     }
 
     @Override
@@ -230,15 +211,6 @@ public class SmppServerResourceAdaptor implements ResourceAdaptor {
             smscManagemet.stopSmppManagement();
         } catch (Exception e) {
             tracer.severe("Error while inactivating RA Entity " + this.raContext.getEntityName(), e);
-        }
-
-        try {
-            if(loadBalancerHeartBeatingService != null) {
-                loadBalancerHeartBeatingService.stop();
-                loadBalancerHeartBeatingService = null;
-            }
-        } catch (Exception e) {
-            tracer.severe("Error while stopping RAs LB heartbeating service " + this.raContext.getEntityName(), e);
         }
         if (tracer.isInfoEnabled()) {
             tracer.info("Inactivated RA Entity " + entityName);
@@ -253,173 +225,10 @@ public class SmppServerResourceAdaptor implements ResourceAdaptor {
 
     @Override
     public void raUnconfigure() {
-        loadBalancerHeartBeatingServiceProperties = null;
     }
 
     @Override
     public void raVerifyConfiguration(ConfigProperties properties) throws InvalidConfigurationException {
-        try {
-            checkAddressAndPortProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_PORT, properties);
-            checkAddressAndPortProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_SSL_PORT, properties);
-            checkBalancersProperty(properties);
-            checkHeartBeatingServiceClassNameProperty(properties);
-        } catch (Throwable e) {
-            throw new InvalidConfigurationException(e.getMessage(), e);
-        }
-    }
-
-    private void checkAddressAndPortProperty(String portPropertyName, ConfigProperties properties) throws IOException {
-        ConfigProperties.Property property = properties.getProperty(portPropertyName);
-        if (property != null) {
-            Integer localHttpPort = (Integer) property.getValue();
-            if (localHttpPort != null && localHttpPort != -1) {
-                String localHttpAddress = (String) properties.getProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_ADDRESS)
-                        .getValue();
-                if (localHttpAddress != null && !localHttpAddress.isEmpty()) {
-                    InetSocketAddress sockAddress = new InetSocketAddress(localHttpAddress, localHttpPort);
-                 // bypassing it now because stack is nor running this time
-//                    checkSocketAddress(sockAddress);
-                }
-            }
-        }
-    }
-
-    private void checkSocketAddress(InetSocketAddress address) throws IOException {
-        Socket s = new Socket();
-        try {
-            s.connect(address);
-        } finally {
-            s.close();
-        }
-    }
-
-    private void checkBalancersProperty(ConfigProperties properties) {
-        String balancers = (String) properties.getProperty(SmppLoadBalancerHeartBeatingService.BALANCERS).getValue();
-        if (balancers != null && !balancers.isEmpty()) {
-            String[] segments = balancers.split(SmppLoadBalancerHeartBeatingServiceImpl.BALANCERS_CHAR_SEPARATOR);
-            for (String segment : segments) {
-                String[] addressAndPort = segment.split(SmppLoadBalancerHeartBeatingServiceImpl.BALANCER_PORT_CHAR_SEPARATOR);
-                Integer.parseInt(addressAndPort[1]);
-            }
-        }
-    }
-
-    private void checkHeartBeatingServiceClassNameProperty(ConfigProperties properties) throws ClassNotFoundException {
-        String httpBalancerHeartBeatServiceClassName = (String) properties
-                .getProperty(SmppLoadBalancerHeartBeatingService.LB_HB_SERVICE_CLASS_NAME).getValue();
-        if (httpBalancerHeartBeatServiceClassName != null && !httpBalancerHeartBeatServiceClassName.isEmpty()) {
-            Class.forName(httpBalancerHeartBeatServiceClassName);
-        }
-    }
-
-    private SmppLoadBalancerHeartBeatingService initHeartBeatingService(MBeanServer mBeanServer) throws Exception {
-        int smppBindPort = SmppManagement.getInstance().getSmppServerManagement().getBindPort();
-        loadBalancerHeartBeatingServiceProperties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_PORT,
-                String.valueOf(smppBindPort));
-        String httpBalancerHeartBeatServiceClassName = (String) loadBalancerHeartBeatingServiceProperties
-                .getProperty(SmppLoadBalancerHeartBeatingService.LB_HB_SERVICE_CLASS_NAME);
-        SmppLoadBalancerHeartBeatingService service = (SmppLoadBalancerHeartBeatingService) Class
-                .forName(httpBalancerHeartBeatServiceClassName).newInstance();
-
-        String stackName = this.raContext.getEntityName();
-        service.init(this.raContext, mBeanServer, stackName, loadBalancerHeartBeatingServiceProperties);
-        return service;
-    }
-
-    private Properties prepareHeartBeatingServiceProperties(ConfigProperties configProperties)
-            throws InvalidConfigurationException {
-        Properties properties = null;
-
-        String balancers = prepareProperty(configProperties, SmppLoadBalancerHeartBeatingServiceImpl.BALANCERS);
-        String sslPort = localSslPortProperty(configProperties);
-        String localAddress = prepareLocalHttpAddressProperty(configProperties);
-        String heartbeatInterval = prepareProperty(configProperties,
-                SmppLoadBalancerHeartBeatingServiceImpl.HEARTBEAT_INTERVAL);
-        String lbHeartbeatingServiceClassName = prepareProperty(configProperties,
-                SmppLoadBalancerHeartBeatingServiceImpl.LB_HB_SERVICE_CLASS_NAME);
-
-        if (balancers != null && !balancers.isEmpty()) {
-
-            if (localAddress == null || lbHeartbeatingServiceClassName == null) {
-                StringBuilder sb = new StringBuilder();
-                sb.append("Invalid loadbalancer configuration. One of required properties if missing. ");
-                sb.append("Required properties:\n");
-                sb.append(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_ADDRESS).append("\n");
-                sb.append(SmppLoadBalancerHeartBeatingServiceImpl.LB_HB_SERVICE_CLASS_NAME).append("\n");
-                sb.append("Missing properties:").append("\n");
-                if (localAddress == null) {
-                    sb.append(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_ADDRESS).append("\n");
-                }
-                if (lbHeartbeatingServiceClassName == null) {
-                    sb.append(SmppLoadBalancerHeartBeatingServiceImpl.LB_HB_SERVICE_CLASS_NAME).append("\n");
-                }
-                throw new InvalidConfigurationException(sb.toString());
-            }
-
-            properties = new Properties();
-            properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.BALANCERS, balancers);
-            properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_ADDRESS, localAddress);
-            properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LB_HB_SERVICE_CLASS_NAME,
-                    lbHeartbeatingServiceClassName);
-            if (heartbeatInterval != null) {
-                properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.HEARTBEAT_INTERVAL, heartbeatInterval);
-            }
-            if (sslPort != null) {
-                properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_SSL_PORT, sslPort);
-            }
-
-        } else if (localAddress != null || lbHeartbeatingServiceClassName != null) {
-            balancers = "";
-            properties = new Properties();
-            properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.BALANCERS, balancers);
-            properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_ADDRESS, localAddress);
-            properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LB_HB_SERVICE_CLASS_NAME,
-                    lbHeartbeatingServiceClassName);
-            if (heartbeatInterval != null) {
-                properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.HEARTBEAT_INTERVAL, heartbeatInterval);
-            }
-            if (sslPort != null) {
-                properties.setProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_SSL_PORT, sslPort);
-            }
-        }
-
-        return properties;
-    }
-
-    private String localSslPortProperty(ConfigProperties configProperties) {
-        String propertyValue = null;
-        ConfigProperties.Property sslPortProperty = configProperties
-                .getProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_SSL_PORT);
-        if (sslPortProperty != null && sslPortProperty.getValue() != null) {
-            Integer intValue = (Integer) sslPortProperty.getValue();
-            if (intValue > 0) {
-                propertyValue = String.valueOf(sslPortProperty.getValue());
-            }
-        }
-        return propertyValue;
-    }
-
-    private String prepareLocalHttpAddressProperty(ConfigProperties configProperties) {
-        ConfigProperties.Property localAddressProperty = configProperties
-                .getProperty(SmppLoadBalancerHeartBeatingServiceImpl.LOCAL_ADDRESS);
-        String localHttpAddress = null;
-        if (localAddressProperty == null || localAddressProperty.getValue() == null
-                || ((String) localAddressProperty.getValue()).isEmpty()) {
-            localHttpAddress = getJBossAddress();
-        } else {
-            localHttpAddress = (String) localAddressProperty.getValue();
-        }
-        return localHttpAddress;
-    }
-
-    private String prepareProperty(ConfigProperties configProperties, String propertyName) {
-        String propertyValue = null;
-        ConfigProperties.Property configProperty = configProperties
-                .getProperty(propertyName);
-        if (configProperty != null && configProperty.getValue() != null) {
-            propertyValue = String.valueOf(configProperty.getValue());
-        }
-        return propertyValue;
     }
 
     @Override
@@ -540,24 +349,4 @@ public class SmppServerResourceAdaptor implements ResourceAdaptor {
 	public SmppServerResourceAdaptorStatisticsUsageParameters getStatisticsUsageParameterSet() {
 	    return this.defaultUsageParameters;
 	}
-
-	private String getJBossAddress() {
-        String address = null;
-        Object inetAddress = null;
-        try {
-            inetAddress = ManagementFactory.getPlatformMBeanServer().getAttribute(new ObjectName("jboss.as:interface=public"),
-                    "inet-address");
-        } catch (Exception e) {
-        }
-
-        if (inetAddress != null) {
-            address = inetAddress.toString();
-            tracer.info("inet-address is found from jboss.as:interface=public: " + address);
-        } else {
-            address = System.getProperty("jboss.bind.address");
-            tracer.info("inet-address is found from jboss.bind.address: " + address);
-        }
-
-        return address;
-    }
 }
